50dbefa56084556b262a92a65e277000eb8eb7aa
Jephtebb <jephteborno@yahoo.com>
2020-12-24 05:58:34 -0500

    refactor the algorithm to play game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,70 @@
  let numbers = [];
  let displayInfo = document.querySelector('#display-info');
-powerball = ()=>{
-     let numbersContainer = document.createElement('div'); 
-    numbersContainer.classList.add('data'); 
-    for (let i = 0; i<5; i++){  
+class LuckyNumbers {
+    constructor(balls, length){
+    this.balls =balls;
+    this.length = length;
+    }
+    randomNumbers(){
+        for (let i =0; i<this.balls;i++){
+            let numberDraw = Math.floor(Math.random()*this.length+1);
+            // if (numbers.includes(numberDraw)){
+            //     i--;
+            // }else{
 
-        numberDraw = Math.floor(Math.random()*69 +1);
-        if (numbers.includes(numberDraw)){
-            i--;
-        }else{
-            numbers.push(numberDraw);
-            var numberContainer = document.createElement('p');
-            numberContainer.classList.add('numberStyle');
-            numberContainer.textContent = `${numberDraw}`;
-            numbersContainer.appendChild(numberContainer);
+            
+                let numbersContainer = document.createElement('div');
+                // numbersContainer.classList.add('numberStyle');
+                let displayData = document.createElement('div');
+                numbers.push(numberDraw);
+                var numberContainer = document.createElement('p');
+                numberContainer.classList.add('numberStyle');
+                numberContainer.textContent = `${numberDraw}`;
+                numbersContainer.appendChild(numberContainer);
+                displayData.appendChild(numbersContainer);
+                displayInfo.appendChild(displayData);
+            // }
+            console.log(this.balls);
+            console.log(i)
         }
     }
-    let PB = Math.floor(Math.random()*26+1);
-    let PBdata = document.createElement('p')
-    let PBdataDiv  = document.createElement('div');
-    PBdataDiv.classList.add('data')
-    PBdata.classList.add('numberStyle');
-    PBdata.textContent = PB;
-    PBdataDiv.appendChild(PBdata);
-    let displayInfo = document.querySelector('#display-info');
-    let displayData = document.createElement('div');
-    var Pb = document.createElement('div');
-    Pb.textContent = "and your power ball number is ";
-    displayData.textContent = `Your powerball numbers are: `;
-    displayData.appendChild(numbersContainer);
-    displayData.appendChild(Pb);
-    displayData.appendChild(PBdataDiv);
-    displayInfo.appendChild(displayData);
-}
-megaMillions = ()=>{
-    for (let i = 0; i<5; i++){
-        numberDraw = Math.floor(Math.random()*70 +1);
-        if (numbers.includes(numberDraw)){
-            i--;
-        }else{
-        numbers.push(numberDraw);
-    
-        }
+    extraNumber(ball){
+        let exNumber = Math.floor(Math.random()*ball+1); 
+        let exdata = document.createElement('p');
+        exdata.textContent = exNumber;
+        exdata.classList.add('numberStyle');
+        displayInfo.appendChild(exdata);
     }
-    let MB = Math.floor(Math.random()*25+1);
-
-    let displayInfo = document.querySelector('#display-info');
-    let displayData = document.createElement('div');
-    displayData.textContent = `Your mega Millions numbers are: ${numbers[0]} ${numbers[1]} ${numbers[2]} ${numbers[3]} ${numbers[4]} 
-                   and your PB is: ${MB}`;
-    displayInfo.appendChild(displayData);
 }
+let powerballObject = new LuckyNumbers(5,69);
+let powerball =document.querySelector('#powerball');
+powerball.addEventListener('click', function(){
+    displayInfo.innerHTML = "";
+   
+    powerballObject.randomNumbers();
+    powerballObject.extraNumber(26);
+})
 
-floridaLotto = ()=>{
-    for (let i = 0; i<6; i++){
-        numberDraw = Math.floor(Math.random()*53 +1);
-        if (numbers.includes(numberDraw)){
-            i--;
-        }else{
-        numbers.push(numberDraw);
-    
-        }
-    }
+let megaObject = new LuckyNumbers(5,70);
+let mega =document.querySelector('#megaMillions');
+mega.addEventListener('click', function(){
+    displayInfo.innerHTML = "";
+    megaObject.randomNumbers();
+    megaObject.extraNumber(25);
+})
 
-    let displayInfo = document.querySelector('#display-info');
-    let displayData = document.createElement('div');
-    displayData.textContent = `Your florida lotto numbers are: ${numbers[0]} ${numbers[1]} ${numbers[2]} ${numbers[3]} ${numbers[4]} ${numbers[5]} `
-    displayInfo.appendChild(displayData);
-}
+let flLottoObject = new LuckyNumbers(6,53);
+let floridaLotto =document.querySelector('#floridaLotto');
+floridaLotto.addEventListener('click', function(){
+    displayInfo.innerHTML = "";
+    flLottoObject.randomNumbers();
+})
 
-fantasy5 = ()=>{
-    for (let i = 0; i<5; i++){
-        numberDraw = Math.floor(Math.random()*36 +1);
-        if (numbers.includes(numberDraw)){
-            i--;
-        }else{
-        numbers.push(numberDraw);
-     
-        }
-    }
-
-    let displayInfo = document.querySelector('#display-info');
-    let displayData = document.createElement('div');
-    displayData.textContent = `Your fantasy 5 numbers are: ${numbers[0]} ${numbers[1]} ${numbers[2]} ${numbers[3]} ${numbers[4]}`
-    displayInfo.appendChild(displayData);
-}
-
-//Events Listeners Start
-var selectPowerball = document.querySelector('#powerball');
-selectPowerball.addEventListener('click',powerball);
-
-
-var selectMega = document.querySelector('#megaMillions');
-selectMega.addEventListener('click',megaMillions);
-
-
-var selectFlorida = document.querySelector('#floridaLotto');
-selectFlorida.addEventListener('click',floridaLotto);
-
-var selectFantasy = document.querySelector('#fantasy5');
-selectFantasy.addEventListener('click',fantasy5);
-//Events Listeners End
+let fantasy5Object = new LuckyNumbers(5,36);
+let fantasy5 =document.querySelector('#fantasy5');
+fantasy5.addEventListener('click', function(){
+    displayInfo.innerHTML = "";
+    fantasy5Object.randomNumbers();
+})
 
 
